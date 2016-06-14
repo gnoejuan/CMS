f1256007bfed76087c8bf5834398a60b7c431ed0
Charles  Rodriguez <provenhypothesis@gmail.com>
2016-06-14 18:29:40 -0500

    intial commit. Lets get collaborating

---
+++ CMS/Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Models
+{
+    class Blog
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime PublishedDate { get; set; }
+        public Boolean isPublic { get; set; }
+    }
+}
